5a1e1256f706d6ea70bbfc7590ad9d50d8211f64
Shahruk Khan <shahruksemail@gmail.com>
2013-03-31 23:44:31 -0400

    Test

--- app/Config/core.php
+++ app/Config/core.php
@@ -287,6 +287,6 @@ Configure::write('Facebook.secret', '616842b8158d5beb8a0d0ee771dfe9d0');
 if(stristr(env('HTTP_HOST'), 'boxngo.local')){ 
     Configure::write('debug', 2); 
 }else{ 
-    Configure::write('debug', 0); 
+    Configure::write('debug', 2); 
 }
 
