17db158d5a9263e45bd48c93dab778cc751e7622
Shahruk Khan <shahruksemail@gmail.com>
2013-03-31 23:40:32 -0400

    API test

--- app/Model/Facebook.php
+++ app/Model/Facebook.php
@@ -8,8 +8,16 @@
 			App::uses('HttpSocket', 'Network/Http');
 			$this->httpSocket = new HttpSocket();
 			parent::__construct($id, $table, $ds);
-			
+
+			App::import('Lib', 'Facebook.FB');
+ 
+			//PHP 5.3
+			FB::api('/me'); //graph system call to /me
+			 
+			//PHP < 5.3
+			$this->facebook = new FB();
 		}
+
 		public function shareListing($name,$url,$fbId,$accessToken,$message=NULL, $imageUrl=NULL){
 			$message = 'I just listed '.$name.' for sale on BOX\'NGO! Check it out!';
 			$postUrl = 'https://graph.facebook.com/'.$fbId.'/feed?picture='.urlencode($imageUrl).'&link='.urlencode($url).'&name='.$name.'&message='.urlencode($message).'&access_token='. $accessToken;
@@ -23,10 +31,15 @@
 			$data = array('access_token' => $accessToken, 'object' => $productUrl);
 			$results = $this->httpSocket->post($postUrl, $data);
 
-			$getUrl = 'https://graph.facebook.com/116996495106723/theboxngo:favorite&access_token='.$accessToken.'&method=POST&gifts_product='.$productUrl;
-			$results2 = $this->httpSocket->post($getUrl);
+			$response = $facebook->api(
+				'me/theboxngo:favorite'
+				'POST',
+				array(
+					'gifts_product' => $productUrl
+				)
+			);
 			print_r($getUrl);
-			print_r($results2);
+			print_r($response);
 			die();
 			return $results;
 		}
