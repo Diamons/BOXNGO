d6d1d9ac3f12b6304d20d386880ddba0674b1f1c
Shahruk Khan <Shahruksemail@gmail.com>
2013-04-11 04:18:25 -0400

    Working on requests

---
+++ app/Controller/RequestsController.php
@@ -0,0 +1,12 @@
+<?php
+	class RequestsController extends AppController{
+
+		public function beforeFilter(){
+			parent::beforeFilter();
+			$this->Auth->allow('index');
+		}
+
+		public function index(){
+
+		}
+	}
---
+++ app/View/Requests/index.ctp
@@ -0,0 +1,5 @@
+<div id="content" class="wrapper">
+	<div class="request">
+		<h2>A [penis]?</h2>
+	</div>
+</div>
