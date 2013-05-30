8ee361942469b6615e10b3e791011860d24e479e
Shahruk Khan <Shahruksemail@gmail.com>
2013-05-29 23:42:10 -0400

    new forums

---
+++ app/webroot/forums/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysql';
+$db_host = 'us-cdbr-east-03.cleardb.com';
+$db_name = 'heroku_5648c9c067c6638';
+$db_username = 'bf9d3d7e7eb560';
+$db_password = 'aadf777b';
+$db_prefix = 'forums_';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_dc65f0';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = 'd8b25921091536ed';
+
+define('PUN', 1);
