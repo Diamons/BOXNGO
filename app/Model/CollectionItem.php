4e4a50aadc4e638bc5a5eacdcaeacd885ae8fde7
Shahruk Khan <Shahruksemail@gmail.com>
2013-06-10 23:41:26 -0400

    Quickfix

--- app/Model/CollectionItem.php
+++ app/Model/CollectionItem.php
@@ -9,13 +9,8 @@
 			if(isset($this->data[$this->alias]['shop_id'])){
 				$newResults = array();
 				for($i = 0; $i < count($this->data[$this->alias]['shop_id']); $i++){
-					if(empty($this->data[$this->alias]['shop_id'][$i])){
-						unset($this->data[$this->alias]['shop_id'][$i]);
-						$i--;
-					}else{
-						$this->data[$this->alias]['shop_id'][$i] = trim($this->data[$this->alias]['shop_id'][$i]);
-						$newResults[]['shop_id'] = $this->data[$this->alias]['shop_id'][$i];
-					}
+					$newResults = explode(',', trim($this->data[$this->alias]['shop_id']));
+					debug($newResults);
 				}
 				$this->data = $newResults;
 				debug($this->data);
