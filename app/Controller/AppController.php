dbd409f6f5500848c80ae35698761dd3426787fe
Shahruk Khan <Shahruksemail@gmail.com>
2013-08-10 19:18:01 -0400

    CSRF Fix

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -35,7 +35,7 @@ App::uses('Controller', 'Controller');
 App::uses('Sanitize', 'Utility');
 class AppController extends Controller {
 	public $uses = array('Autologin', 'User', 'Category', 'Shop', 'Order', 'School', 'Message', 'Thread', 'NotificationItem');
-	public $components = array('UserLogin', 'Cookie', 'Auth', 'Security', 'Session');
+	public $components = array('UserLogin', 'Cookie', 'Auth', 'Security', => array('csrfUseOnce' => false), 'Session');
 	public $helpers = array('Form');
 
 	public function beforeFilter(){
