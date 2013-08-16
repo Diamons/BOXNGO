9b0dcd32e934e25d1b513761d35d461df8d67ac4
Shahruk Khan <Shahruksemail@gmail.com>
2013-08-15 22:35:09 -0400

    Test added

--- app/Console/Command/CronShell.php
+++ app/Console/Command/CronShell.php
@@ -15,4 +15,16 @@
 				parent::sendEmail("shahruk@theboxngo.com", "BOX'NGO :: Important reminder that you have an order awaiting your acceptance!", "remindseller", array("link" => $orders[$i]['Shop']['full_url'], "imageUrl" => $image['Image']['url'], "totalPrice" => $orders[$i]['Order']['totalPrice'], "name" => $orders[$i]['Shop']['name'], "expire" => $expire->format('Y-m-d h:i:s A')));
 			}
 	    }
+		
+		public function remindTest(){
+			$orders = $this->Order->find("all",array("conditions" => array("Order.status" => "pending", "DATEDIFF(CURDATE(), DATE(Order.created)) <" => 4)));
+			for($i = 0; $i < count($orders); $i++){
+				$expire = new DateTime($orders[$i]['Order']['created'], new DateTimeZone('America/New_York'));
+				$expire = $expire->modify('+4 day');
+				$image = $this->Shop->Image->getShopImage($orders[$i]['Shop']['id']);
+				//parent::sendEmail($orders[$i]['User']['username'], "BOX'NGO :: Important reminder that you have an order awaiting your acceptance!", "remindseller", array("link" => $orders[$i]['Shop']['full_url'], "imageUrl" => $image['Image']['url'], "totalPrice" => $orders[$i]['Order']['totalPrice'], "name" => $orders[$i]['Shop']['name'], "expire" => $expire->format('Y-m-d h:i:s A')));
+				//parent::sendEmail("shahruk@theboxngo.com", "BOX'NGO :: Important reminder that you have an order awaiting your acceptance!", "remindseller", array("link" => $orders[$i]['Shop']['full_url'], "imageUrl" => $image['Image']['url'], "totalPrice" => $orders[$i]['Order']['totalPrice'], "name" => $orders[$i]['Shop']['name'], "expire" => $expire->format('Y-m-d h:i:s A')));
+				$this->out($orders[$i]['User']['username']);
+			}
+	    }
 	}
